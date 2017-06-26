2c91f79ca5604bbb8ca955255f6257c1918f6908
Max Scher <maxobaxo@gmail.com>
2017-06-25 22:01:49 -0700

    refactor shipping calculator using function

--- shipping_calculator.php
+++ shipping_calculator.php
@@ -1,8 +1,13 @@
 <?php
     $weight = $_GET["weight"];
     $distance = $_GET["distance"];
-    $solution = ($weight / 20) + ($distance / 20);
-    $returnString = "Cost ($): " . $solution;
+    $shipping_price = "Cost ($): " . calculateShipping($weight, $distance);
+
+    function calculateShipping($weight, $distance)
+    {
+        $price = ($distance / $weight) + 5;
+        return $price;
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,6 +18,6 @@
     <h1>Shipping Calculator</h1>
     <p>Weight (kg): <?php echo $weight; ?></p>
     <p>Distance (km): <?php echo $distance; ?></p>
-    <p><?php echo $returnString; ?></p>
+    <p><?php echo $shipping_price; ?></p>
 </body>
 </html>
